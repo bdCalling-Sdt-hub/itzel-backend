cde367e667843e82a09ad2b7ad280b872530f255
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-01-24 11:04:52 +0600

    fixing some issues

--- src/helpers/emailHelper.ts
+++ src/helpers/emailHelper.ts
@@ -16,7 +16,7 @@ const transporter = nodemailer.createTransport({
 const sendEmail = async (values: ISendEmail) => {
   try {
     const info = await transporter.sendMail({
-      from: `"Reza " ${config.email.from}`,
+      from: `"914 Unplugged " ${config.email.from}`,
       to: values.to,
       subject: values.subject,
       html: values.html,
