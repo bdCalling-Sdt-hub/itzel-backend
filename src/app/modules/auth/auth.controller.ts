756a8f594a56f66cc0ac70ef9b0bfc09cc75029f
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-02-07 15:40:31 +0600

    fixing reset password

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -41,7 +41,7 @@ const forgetPassword = catchAsync(async (req: Request, res: Response) => {
 });
 
 const resetPassword = catchAsync(async (req: Request, res: Response) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(' ')[1];
   const { ...resetData } = req.body;
   const result = await AuthService.resetPasswordToDB(token!, resetData);
 
